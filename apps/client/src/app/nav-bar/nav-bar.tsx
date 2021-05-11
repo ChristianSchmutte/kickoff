8f31813ddebb4c7e6aa043f2c4ed17bcecc53822
Aric <aricjiang89@gmail.com>
2021-05-11 19:23:58 +0800

    feat: navbar component

---
+++ apps/client/src/app/nav-bar/nav-bar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './nav-bar.module.scss';
+
+import './nav-bar.module.scss';
+
+/* eslint-disable-next-line */
+export interface NavBarProps {}
+
+export function NavBar(props: NavBarProps) {
+  return <div className={styles.container}></div>;
+}
+
+export default NavBar;
