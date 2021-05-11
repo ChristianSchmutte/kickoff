78447ba9145ff0af1531f7c46d48e8dc6514e29d
Aric <aricjiang89@gmail.com>
2021-05-12 02:46:09 +0800

    feat: few changes to nav-bar, and nav-bar stylesheet

--- apps/client/src/app/nav-bar/nav-bar.tsx
+++ apps/client/src/app/nav-bar/nav-bar.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './nav-bar.module.scss';
-
+import home from '../../assets/home.svg';
 import './nav-bar.module.scss';
 
 /* eslint-disable-next-line */
 export interface NavBarProps {}
 
 export function NavBar(props: NavBarProps) {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <img src={home} alt="home icon" />
+    </div>
+  );
 }
 
 export default NavBar;
