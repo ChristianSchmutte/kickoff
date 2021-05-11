0f4361227409d7a0039c23f2dbc154e2712e62bb
Aric <aricjiang89@gmail.com>
2021-05-11 19:23:35 +0800

    feat: add NavBar, add classnames, add id to mockActivities

--- apps/client/src/app/feed/feed.tsx
+++ apps/client/src/app/feed/feed.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import ActivityCard from '../activity-card/activity-card';
+import NavBar from '../nav-bar/nav-bar';
+import styles from './feed.module.scss';
 
 import './feed.module.scss';
 
@@ -7,6 +9,7 @@ import './feed.module.scss';
 export interface FeedProps {}
 
 interface Activity {
+  id: number;
   title: string;
   description: string;
   startTimestamp: number;
@@ -16,29 +19,40 @@ interface Activity {
 
 const mockActivities: Activity[] = [
   {
+    id: 12,
     title: 'Basketball',
     description: 'Stuff about bball',
     startTimestamp: new Date(2021, 4, 12, 11).getTime(),
     endTimestamp: new Date(2021, 4, 13, 13).getTime(),
-    location: 'Middle of nowhere'
-  }
-]
+    location: 'Middle of nowhere',
+  },
+  {
+    id: 8,
+    title: 'Cricket',
+    description: 'Stuff about cricket',
+    startTimestamp: new Date(2021, 4, 15, 12).getTime(),
+    endTimestamp: new Date(2021, 4, 16, 14).getTime(),
+    location: 'somehwere',
+  },
+];
 
 export function Feed(props: FeedProps) {
   const [activities, setActivities] = useState<Activity[]>(mockActivities);
 
   const activityCards: JSX.Element[] = activities.map((activity) => (
-    <ActivityCard 
+    <ActivityCard
+      className={styles.activity}
       title={activity.title}
       description={activity.description}
       startTimestamp={activity.startTimestamp}
       endTimestamp={activity.endTimestamp}
       location={activity.location}
-      key="1"
+      key={activity.id}
     />
   ));
   return (
-    <div>
+    <div className={styles.container}>
+      <NavBar />
       {activityCards}
     </div>
   );
