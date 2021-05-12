ec123b789397d93635f71c18666ea542584dc395
abduljawwad <abduljawwad.md@gmail.com>
2021-05-12 11:51:12 +0100

    feat: create event form component

---
+++ apps/client/src/app/create-event-form/create-event-form.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './create-event-form.module.scss';
+
+/* eslint-disable-next-line */
+export interface CreateEventFormProps {}
+
+export function CreateEventForm(props: CreateEventFormProps) {
+  return (
+    <div>
+      <h1>Welcome to createEventForm!</h1>
+    </div>
+  );
+}
+
+export default CreateEventForm;
