ce1e7638a24f11cbcb854a6a47c424b36bf545b3
Aric <aricjiang89@gmail.com>
2021-05-13 02:57:35 +0800

    remove firebasApp

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,10 +1,7 @@
 import styles from './app.module.scss';
 import Feed from './feed/feed';
-import firebase from 'firebase/app';
 
 export function App() {
-  const firebaseApp = firebase.apps[0];
-  console.log(JSON.stringify(firebaseApp.options, null, 2));
   return <Feed />;
 }
 
