a2518bf0eeed1476df9453b54480eaee66746ff6
Aric <aricjiang89@gmail.com>
2021-05-13 02:56:28 +0800

    feat: auth content component

---
+++ apps/client/src/app/auth-content/auth-content.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './auth-content.module.scss';
+
+/* eslint-disable-next-line */
+export interface AuthContentProps {}
+
+export function AuthContent(props: AuthContentProps) {
+  return (
+    <div>
+      <h1>Welcome to AuthContent!</h1>
+    </div>
+  );
+}
+
+export default AuthContent;
