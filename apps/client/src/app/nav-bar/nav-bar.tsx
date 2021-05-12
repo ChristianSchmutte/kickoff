bdcfd2dc4227bfcd694145d145b7525f5b97427a
Aric <aricjiang89@gmail.com>
2021-05-12 16:49:32 +0800

    add new img jsx element, import new icon

--- apps/client/src/app/nav-bar/nav-bar.tsx
+++ apps/client/src/app/nav-bar/nav-bar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './nav-bar.module.scss';
 import home from '../../assets/home.svg';
 import './nav-bar.module.scss';
-
+import createIcon from '../../assets/plus-circle.svg';
 /* eslint-disable-next-line */
 export interface NavBarProps {}
 
@@ -10,6 +10,11 @@ export function NavBar(props: NavBarProps) {
   return (
     <div className={styles.container}>
       <img src={home} alt="home icon" />
+      <img
+        className={styles.createEventButton}
+        src={createIcon}
+        alt="create event icon"
+      />
     </div>
   );
 }
