820911cb768fc67097a703ad009e179a9136962b
Aric <aricjiang89@gmail.com>
2021-05-12 18:55:14 +0800

    create login component

---
+++ apps/client/src/app/login/login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './login.module.scss';
+
+/* eslint-disable-next-line */
+export interface LoginProps {}
+
+export function Login(props: LoginProps) {
+  return (
+    <div>
+      <h1>Welcome to login!</h1>
+    </div>
+  );
+}
+
+export default Login;
