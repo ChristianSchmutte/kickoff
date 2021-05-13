cd67a2dfe0df5056ce062a0f0f84e329a532e113
abduljawwad <abduljawwad.md@gmail.com>
2021-05-14 00:07:39 +0100

    feat: create activity form component and deleted create event form component(due to naming mismatch with activity card component)

---
+++ apps/client/src/app/create-activity-form/create-activity-form.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './create-activity-form.module.scss';
+
+/* eslint-disable-next-line */
+export interface CreateActivityFormProps {}
+
+export function CreateActivityForm(props: CreateActivityFormProps) {
+  return (
+    <div>
+      <h1>Welcome to createActivityForm!</h1>
+    </div>
+  );
+}
+
+export default CreateActivityForm;
