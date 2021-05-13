69aa5a43462a2b6f69bfd2049c130fa5d57e2773
Aric <aricjiang89@gmail.com>
2021-05-13 19:30:32 +0800

    feat: add signUp component

---
+++ apps/client/src/app/sign-up/sign-up.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './sign-up.module.scss';
+
+/* eslint-disable-next-line */
+export interface SignUpProps {}
+
+export function SignUp(props: SignUpProps) {
+  return (
+    <div>
+      <h1>Welcome to signUp!</h1>
+    </div>
+  );
+}
+
+export default SignUp;
