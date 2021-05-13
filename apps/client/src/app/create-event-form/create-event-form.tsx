2badfec6dc95040b61ac1bd0479dadf8b3580d67
abduljawwad <abduljawwad.md@gmail.com>
2021-05-13 21:15:31 +0100

    Co-authored-by: ninjarogue <ninjarogue@users.noreply.github.com>

--- apps/client/src/app/create-event-form/create-event-form.tsx
+++ apps/client/src/app/create-event-form/create-event-form.tsx
@@ -8,11 +8,7 @@ export interface CreateEventFormProps {}
 export function CreateEventForm(props: CreateEventFormProps) {
   return (
     <div>
-<<<<<<< HEAD
       <h1>Welcome to createEventForm!</h1>
-=======
-      <h1>Welcome to CreateEventForm!</h1>
->>>>>>> e3e139fd8abf55a20906527d1cc162bce00867a2
     </div>
   );
 }
