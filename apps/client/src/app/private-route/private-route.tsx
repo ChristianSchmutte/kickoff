98b3c250eba27c4b1051618ca9b7d7dca1a28da6
Aric <aricjiang89@gmail.com>
2021-05-13 23:29:25 +0800

    feat: PrivateRoute component

--- apps/client/src/app/private-route/private-route.tsx
+++ apps/client/src/app/private-route/private-route.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { useAuth } from '../auth-content/auth-content';
 import { Route, Redirect } from 'react-router-dom';
 import './private-route.module.scss';
 
 /* eslint-disable-next-line */
-export interface PrivateRouteProps {}
+export interface PrivateRouteProps {
+  component: FunctionComponent;
+  path: string;
+  exact: boolean;
+}
 
-const PrivateRoute: React.FC = (props: PrivateRouteProps) => {
+const PrivateRoute: FunctionComponent<PrivateRouteProps> = ({
+  component: RouteComponent,
+  exact,
+  path
+}) => {
   const { user } = useAuth();
 
   return user ? (
-    <Route path={props.path} exact={props.exact} component={props.component} />
+    <Route path={path} exact={exact} component={RouteComponent} />
   ) : (
     <Redirect to='/login' />
   );
