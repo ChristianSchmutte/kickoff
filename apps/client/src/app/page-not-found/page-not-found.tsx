73da174ead15e37d7e068363e32110d828606e3c
Aric <aricjiang89@gmail.com>
2021-05-13 23:34:40 +0800

    feat: add PageNotFound component

---
+++ apps/client/src/app/page-not-found/page-not-found.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './page-not-found.module.scss';
+
+/* eslint-disable-next-line */
+export interface PageNotFoundProps {}
+
+export function PageNotFound(props: PageNotFoundProps) {
+  return (
+    <div>
+      <h1>Welcome to PageNotFound!</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
