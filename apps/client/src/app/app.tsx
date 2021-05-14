d1c1aeef908df3942c3850f1d01007324900f2a0
Aric <aricjiang89@gmail.com>
2021-05-14 19:30:37 +0800

    fix: stash

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,10 +1,9 @@
 import styles from './app.module.scss';
 import Feed from './feed/feed';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import PrivateRoute from './private-route/private-route';
-import PageNotFound from './page-not-found/page-not-found';
+import { BrowserRoute as Router, Route, Switch } from 'react-router-dom';
 import Login from './login/login';
 import SignUp from './sign-up/sign-up';
+import PageNotFound from './page-not-found/page-not-found';
 
 // TODO: create PageNotFound Component
 export function App() {
