97bcdf6edad362eb3c5c274f7550ef7b1f98b125
Aric <aricjiang89@gmail.com>
2021-05-14 15:06:05 +0800

    remove all firebase authentication implementation

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,6 +1,5 @@
 import styles from './app.module.scss';
 import Feed from './feed/feed';
-import { AuthProvider } from './auth-content/auth-content';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './private-route/private-route';
 import PageNotFound from './page-not-found/page-not-found';
@@ -10,16 +9,14 @@ import SignUp from './sign-up/sign-up';
 // TODO: create PageNotFound Component
 export function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Switch>
-          <PrivateRoute exact path='/home' component={Feed} />
-          <Route exact path='/' component={Login} />
-          <Route exact path='/signup' component={SignUp} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Switch>
+        <PrivateRoute exact path='/home' component={Feed} />
+        <Route exact path='/' component={Login} />
+        <Route exact path='/signup' component={SignUp} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </Router>
   );
 }
 
