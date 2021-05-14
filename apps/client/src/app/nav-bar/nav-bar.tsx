6a34c2f1360cfb0268642d782b03f2ad15e264e8
Aric <aricjiang89@gmail.com>
2021-05-14 15:24:48 +0800

    remove create event button from navbar

--- apps/client/src/app/nav-bar/nav-bar.tsx
+++ apps/client/src/app/nav-bar/nav-bar.tsx
@@ -2,22 +2,13 @@ import React from 'react';
 import styles from './nav-bar.module.scss';
 import home from '../../assets/home.svg';
 import './nav-bar.module.scss';
-import createIcon from '../../assets/plus-circle.svg';
 /* eslint-disable-next-line */
-export interface NavBarProps {
-  (): void;
-}
+export interface NavBarProps {}
 
-export function NavBar({ clickHandler }) {
+export function NavBar(props: NavBarProps) {
   return (
     <div className={styles.container}>
       <img src={home} alt='home icon' />
-      <img
-        className={styles.createEventButton}
-        src={createIcon}
-        alt='create event icon'
-        onClick={() => clickHandler()}
-      />
     </div>
   );
 }
