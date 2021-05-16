408ab486201376d2f2e031e79ac1f4e4957401b8
abduljawwad <abduljawwad.md@gmail.com>
2021-05-16 19:11:54 +0100

    generate getStarted component

---
+++ apps/client/src/app/get-started/get-started.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './get-started.module.scss';
+
+/* eslint-disable-next-line */
+export interface GetStartedProps {}
+
+export function GetStarted(props: GetStartedProps) {
+  return (
+    <div>
+      <h1>Welcome to getStarted!</h1>
+    </div>
+  );
+}
+
+export default GetStarted;
