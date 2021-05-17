e7ce2129b0b18cea9464bcfdd3191e39cda184b7
Aric <aricjiang89@gmail.com>
2021-05-17 13:42:21 +0800

    feat: wrap QueryClientProvider around app children

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,4 +1,5 @@
 import styles from './app.module.scss';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Feed from './feed/feed';
 import Login from './login/login';
@@ -8,18 +9,21 @@ import ActivityContext from './activity-context/activity-context';
 import PageNotFound from './page-not-found/page-not-found';
 
 export function App() {
+  const queryClient = new QueryClient();
   return (
-    <ActivityContext>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/home' component={Feed} />
-          <Route exact path='/create' component={CreateActivityForm} />
-          <Route exact path='/' component={Login} />
-          <Route exact path='/signup' component={SignUp} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </BrowserRouter>
-    </ActivityContext>
+    <QueryClientProvider client={queryClient}>
+      <ActivityContext>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/home' component={Feed} />
+            <Route exact path='/create' component={CreateActivityForm} />
+            <Route exact path='/' component={Login} />
+            <Route exact path='/signup' component={SignUp} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </BrowserRouter>
+      </ActivityContext>
+    </QueryClientProvider>
   );
 }
 
