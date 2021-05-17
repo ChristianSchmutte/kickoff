93b2c74df9543e8a56cfa00bb0980b560e1cab41
Aric <aricjiang89@gmail.com>
2021-05-17 19:15:43 +0800

    feat: api service for CreateActivityForm

---
+++ apps/client/src/app/create-activity-form/create-activity-form.service.ts
@@ -0,0 +1,24 @@
+import { Activity } from './create-activity-form.types';
+import axios, { AxiosResponse } from 'axios';
+import { MutateFunction } from 'react-query';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333/api'
+});
+const postActivity = async ({
+  endpoint,
+  activity
+}: {
+  endpoint: string;
+  activity: Activity;
+}) => {
+  try {
+    const response = await api.post(endpoint, activity);
+    console.log({ 'axios response': response });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default postActivity;
