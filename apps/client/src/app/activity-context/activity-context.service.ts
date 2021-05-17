1fd7b13cced6ccbdd56e34c31d651cfdde89ab2d
Aric <aricjiang89@gmail.com>
2021-05-18 03:21:33 +0800

    feat: import activity type interface, axios, AxiosResponse type, and AxiosInstance type

--- apps/client/src/app/activity-context/activity-context.service.ts
+++ apps/client/src/app/activity-context/activity-context.service.ts
@@ -1,48 +1,21 @@
-import { useQuery, UseQueryResult } from 'react-query';
-import axios, { AxiosInstance } from 'axios';
-
-interface User {
-  id: number;
-  firstname: string;
-  lastname: string;
-}
-
-interface Location {
-  id: number;
-  name: string;
-  latitude: number;
-  longitude: number;
-}
-
-interface Feed {
-  id: number;
-  title: string;
-  description: string;
-  organizerId: number;
-  locationId: number;
-  sportId: number;
-  timestamp: number;
-  ends: number;
-  participants: User;
-  organizer: User;
-  location: Location;
-}
+import { Activity } from './activity-contex.types';
+import axios, { AxiosResponse, AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
   baseURL: 'http://localhost:3333/api'
 });
-
-const fetchData = async (endpoint: string): Promise<Feed[]> => {
+export const postActivity = async ({
+  endpoint,
+  activity
+}: {
+  endpoint: string;
+  activity: Activity;
+}): Promise<AxiosResponse> => {
   try {
-    const { data } = await api.get(endpoint);
-    return data;
+    const response = await api.post(endpoint, activity);
+    console.log({ 'axios post response': response });
+    return response.data;
   } catch (error) {
-    throw new Error(error.message);
+    console.log(error);
   }
 };
-
-export default function useRequest(
-  endpoint: string
-): UseQueryResult<Feed[], Error> {
-  return useQuery(['feed', endpoint], () => fetchData(endpoint));
-}
