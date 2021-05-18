11cb2d31d2285b7ab78a9de9f9407ee8c70f6dc8
abduljawwad <abduljawwad.md@gmail.com>
2021-05-18 15:46:25 +0100

    feat: generated mapComponent, installed dependencies, mapbox import in main.tsx & session input in .env

---
+++ apps/client/src/app/map-component/map-component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './map-component.module.scss';
+
+/* eslint-disable-next-line */
+export interface MapComponentProps {}
+
+export function MapComponent(props: MapComponentProps) {
+  return (
+    <div>
+      <h1>Welcome to MapComponent!</h1>
+    </div>
+  );
+}
+
+export default MapComponent;
